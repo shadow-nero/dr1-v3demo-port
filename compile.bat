c0e013ccd5aaed11dc9990543a3e96a7f7cb7cb0
morgana-x <89588301+morgana-x@users.noreply.github.com>
2025-09-19 05:42:02 +1000

    Improve folder naming convention, Add more scripts, script improvements, Make ID 18 Kaedes ID (Temp), Temporary Name entry in tex_cmn_name.pak for Kaede

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-Tools\lin_compiler scripts\scripts_source scripts\scripts_edited
+Tools\lin_compiler scripts\scripts_source scripts\scripts_compiled
 Tools\lin_compiler scripts\scripts_source %RELOADEDIIMODS%\dr1.script.demov3\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\script
 pause
