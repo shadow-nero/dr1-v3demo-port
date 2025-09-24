8225a3dca77199b3f1690ad3a865c2419ec9d53c
morgana-x <morgana.is.sugoi@gmail.com>
2025-09-25 02:42:12 +1000

    WIP Texture, First Flash Event, Room edit, Set time to Unknown

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 Tools\lin_compiler scripts\scripts_source scripts\scripts_compiled
-Tools\lin_compiler scripts\scripts_source %RELOADEDIIMODS%\dr1.script.demov3\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\script
 Tools\lin_compiler scripts\scripts_source dr1.script.demov3\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\script
+robocopy dr1.script.demov3 %RELOADEDIIMODS%\dr1.script.demov3 /E
 pause
